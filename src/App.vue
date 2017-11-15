bb89ca738931c2959196a5d0e57c9b50d35d0dcb
Tanguy SCHOLTES <soultaker720@hotmail.com>
2017-11-15 09:36:08 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <h1 class="site-title">Star Wars Api</h1>
+
+        <footer class="footer">
+            <p>This exercice uses the API at <a href="https://swapi.co/">SWAPI.co</a></p>
+            <p><a href="https://vuejs.org/v2/guide/transitions.html">Read Vue Transitions Guide</a></p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+    }
+</script>
+
+<style lang="scss">
+    @font-face {
+        font-family: 'Starjedi';
+        src: url('/src/assets/fonts/starjedi/starjedi.ttf')  format('truetype'); /* Safari, Android, iOS */
+    }
+    .footer, .site-title{
+        text-align: center;
+    }
+    .site-title{
+        font-family: "Starjedi", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        font-size: 3em;
+    }
+</style>
